7a6dc4e005cef65a9a4b51b22640d6ba89f22005
Hutomo Sugianto <hutomo@detik.com>
2015-10-22 08:26:49 +0700

    PR MMall Django

--- MMall/urls.py
+++ MMall/urls.py
@@ -1,4 +1,4 @@
-"""MMall URL Configuration
+"""MMMall URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.conf import settings
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT}),
 ]
--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,36 +1,39 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.template.defaultfilters import slugify
+
+from django_enumfield import enum
+
+# Create your models here.
+from django.utils.text import slugify
 
 
 class Brand(models.Model):
     name = models.CharField(max_length=255)
-
     def __str__(self):
         return self.name
 
-
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    picture = models.ImageField(blank=True, upload_to = 'pictures/%Y/%m/%d')
     slug = models.SlugField(blank=True)
-    #picture = models.ImageField()
-    price = models.DecimalField(decimal_places=2, max_digits=24, default=0)
     brand = models.ForeignKey(Brand)
     category = models.ForeignKey('Category')
     created_by = models.ForeignKey(User)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def image_thumb(self):
+        return '<img src="/media/%s" width="100" height="100" />' % (self.picture)
+    image_thumb.allow_tags = True
+
     def save(self, *args, **kwargs):
         if not self.id:
-            # Newly created object, so set slug
             self.slug = slugify(self.title)
 
         super(Product, self).save(*args, **kwargs)
 
     def __str__(self):
-        return "%s (%s) - %s" % (self.title, self.brand.name, self.category.name)
-
+        return self.title + ' ' + self.brand.name
 
 class Category(models.Model):
     name = models.CharField(max_length=128)
@@ -39,11 +42,29 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class CartStatus(enum.Enum):
+    CREATED = 1
+    PAID = 2
+    SHIPPED = 3
+    DELIVERED = 4
+    DONE = 5
 
 class Cart(models.Model):
-    # Homework
-    pass
+    created_at = models.DateTimeField(auto_now_add=True)
+    customer_name = models.CharField(max_length=255)
+    customer_address = models.CharField(max_length=255)
+    shipping_address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    total = models.IntegerField()
+    payment_date = models.DateTimeField(blank=True)
+    status = enum.EnumField(CartStatus, default=CartStatus.CREATED)
+
+    # def __str__(self):
+    #     return self.id + ' ' + self.brand.name
 
 class CartItem(models.Model):
-    # Homework
-    pass
+    cart_id = models.ForeignKey('Cart')
+    quantity = models.IntegerField()
+    product_id = models.ForeignKey('Product')
+    base_price = models.DecimalField(max_digits=10, decimal_places=2)
+    tax = models.DecimalField(max_digits=10, decimal_places=2)
